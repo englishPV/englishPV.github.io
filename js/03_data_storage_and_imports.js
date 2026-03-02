6fc6fb7201d2d573fb46da89de6f1db5f2d82267
English words <lolmacteur1@gmail.com>
2026-03-02 13:23:10 +0100

    Update 03_data_storage_and_imports.js

--- js/03_data_storage_and_imports.js
+++ js/03_data_storage_and_imports.js
@@ -1,5 +1,5 @@
 /* --- CONSTANTES DATA --- */
-const KEY = 'flashcards9x16_data', APP_VER = '2026-06-22-math-v4';
+const KEY = 'flashcards9x16_data', APP_VER = '2026-06-23-math-v4';
 const GRADES = ['unseen','echec','difficile','bien','facile'];
 const GRADE_INIT = () => Object.fromEntries(GRADES.map(g=>[g,0]));
 const GRADE_FILTERS = () => Object.fromEntries(GRADES.map(g=>[g,!0]));
